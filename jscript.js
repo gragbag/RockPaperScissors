2ca81a6949ad8689e779a3296808a4622851f5e1
Julian Fong <103627958+gragbag@users.noreply.github.com>
2024-09-20 19:30:54 -0700

    Created functions for playRound and playGame
    Added functions for playRound which takes human and computer choice and plays out the round. playGame calls playRound 5 times and prints the result

--- jscript.js
+++ jscript.js
@@ -1,4 +1,7 @@
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
 	const choice = Math.floor(Math.random() * 3);
 	if (choice === 0) {
@@ -11,7 +14,11 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-	const input = prompt("0: Rock, 1: Paper, 2: Scissors");
+	let input = prompt("0: Rock, 1: Paper, 2: Scissors");
+
+	while (isNaN(input) || input < 0 || input > 2) {
+		input = prompt("Enter Valid Number: 0: Rock, 1: Paper, 2: Scissors");
+	}
 
 	if (input === 0) {
 		return "rock";
@@ -22,5 +29,46 @@ function getHumanChoice() {
 	}
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+	if (humanChoice == computerChoice) {
+		console.log("Tie!");
+	} else if (humanChoice === "rock") {
+		if (computerChoice === "scissors") {
+			console.log("You win! Rock beats Scissors");
+			humanScore++;
+		} else {
+			console.log("You lose! Paper beats Rock");
+			computerScore++;
+		}
+	} else if (humanChoice === "paper") {
+		if (computerChoice === "rock") {
+			console.log("You win! Paper beats Rock");
+			humanScore++;
+		} else {
+			console.log("You lose! Scissors beats Paper");
+			computerScore++;
+		}
+	} else {
+		if (computerChoice === "paper") {
+			console.log("You win! Scissors beats Paper");
+			humanScore++;
+		} else {
+			console.log("You lose! Rock beats Scissors");
+			computerScore++;
+		}
+	}
+}
+
+function playGame() {
+	for (let i = 0; i < 5; i++) {
+		const humanChoice = getHumanChoice();
+		const computerChoice = getComputerChoice();
+
+		playRound(humanChoice, computerChoice);
+		console.log("Score:\nHuman: " + humanScore + "  Computer: " + computerScore);
+	}
+}
+
+playGame();
+
+
